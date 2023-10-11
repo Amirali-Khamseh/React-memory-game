0750b8dfd5fd79289c25ca3a954dcdd79e8e67a5
Amir ali <amiralikhamseh01@gmail.com>
2023-10-11 14:59:02 +0200

    State for the user's choices completed as well as checking the match between two choices to set the propper prop for showing the matched items

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Card from './componenets/Card'
 import './App.css';
 
@@ -14,23 +14,63 @@ const cardImages = [
 
 function App() {
 const [cards,setCards] = useState([]); 
-const [turns,setTurns] = useState(0); 
+const [turns,setTurns] = useState(0);
+//States for the Users's choices 
+const [choice1,setChoice1] = useState(null); 
+const [choice2,setChoice2] = useState(null); 
 //Duplicating cards and create the game's grid
 const cardShuffler = ()=>{
   //The sort method shuffle the items when the result of Math.random() is a posetive intiger
   const shuffledCards = [...cardImages,...cardImages]
   .sort(()=>Math.random()-0.5)
-  .map((card)=>({...card,id:Math.random()}));
+  .map((card)=>({...card,id:Math.random()}))
+  .map((card)=>({...card,matched:false}));
   setCards(shuffledCards)
   setTurns(0);
 }
+//Handeling the User's choices
+const handleChoice =(card)=>{
+ 
+  choice1 ? setChoice2(card):setChoice1(card);
+}
+//Comparing selected cards
+useEffect(()=>{
+  if(choice1 && choice2){
+    if(choice1.src === choice2.src){
+      setCards(prevCards =>{
+        return prevCards.map(card=>{
+          if(card.src===choice1.src){
+            return {...card ,matched:true}
+          }else{
+            return card;
+          }
+        })
+      })
+      resetTurn();
+    }else{
+      console.log('not match');
+      resetTurn();
+    }
+  }
+},[choice1,choice2])
+
+// reset choices & increase turn
+  const resetTurn = () => {
+    setChoice1(null)
+    setChoice2(null)
+    setTurns(prevTurns => prevTurns + 1)
+  }
 
   return (
     <div className="App">
-       <h1>Magic Match</h1>
+       <h1>Memory Game</h1>
       <button onClick={cardShuffler}>New Game</button>
       <div className="card-grid">
-    {cards.map((card)=>(<Card key={card.id} card={card}></Card>))}
+    {cards.map((card)=>(<Card key={card.id} 
+    card={card}
+     handleChoice={handleChoice}
+     flipped={card=== choice1 || card === choice2 ||card.matched===true}>
+    </Card>))}
         
       </div>
     </div>
--- src/componenets/Card.js
+++ src/componenets/Card.js
@@ -1,10 +1,13 @@
 import './Card.css';
-const Card = ({card}) =>{
+const Card = ({card,handleChoice ,flipped}) =>{
+    const handleClick = ()=>{
+            handleChoice(card)
+    }
     return(
    <div className='card'>
-      <div>
+      <div className={flipped?'flipped':''}>
         <img className='front' src={card.src} alt="game-cards" />
-        <img className='back' src='/img/cover.png' alt="cover of game-cards" />
+        <img className='back'onClick={handleClick} src='/img/cover.png' alt="cover of game-cards" />
       </div>
     </div>)
 
